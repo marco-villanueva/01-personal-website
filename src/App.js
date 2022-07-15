0f67a8bf19a669c3dea8323a93cb9b07e35e19ff
Marco Villanueva <marco.villanueva@globant.com>
2022-07-15 13:07:18 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Layout from './layouts/Layout';
+import './scss/main.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <h1>Web</h1>
+    </Layout>
   );
 }
 
---
+++ src/layouts/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header'
+
+const Layout = ({ children }) => {
+  return (
+    <div className='o-layout'>
+      <div className='o-layout__container'>
+        <Header/>
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default Layout
