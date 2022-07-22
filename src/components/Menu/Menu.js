46d5e2bacdbade605b14134365d43d07446d6002
Marco Villanueva <marco.villanueva@globant.com>
2022-07-22 14:19:56 -0500

    feature( Menu ): Menu mobile was implemented

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,9 +1,16 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
+import navContext from '../../contexts/navContext';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 
 export const Menu = () => {
 
   const [isDesktop, setDesktop] = useState(window.innerWidth > 900);
+  const [isMenuMobileActive, setIsMenuMobileActive] = useState(false)
+
+  const { scrollDown, sections } = useContext(navContext);
+  const { homeSection, aboutSection, experienceSection, contactSection } = sections;
 
   const updateViewPort = () => {
     setDesktop(window.innerWidth > 900);
@@ -14,23 +21,43 @@ export const Menu = () => {
     return () => window.removeEventListener("resize", updateViewPort);
   });
 
+  const handleBurgerClick = () => {
+    setIsMenuMobileActive(true);
+  }
+
+  const handleMenuOptionClick = (section) => {
+    setIsMenuMobileActive(false);
+    scrollDown(section)
+  }
+
+  const handleCloseMenu = () => {
+    setIsMenuMobileActive(false);
+  }
+
+
   const desktopMenu = (
     <div className='c-desktop-menu'>
-      <button className='c-desktop-menu__item'>home</button>
-      <button className='c-desktop-menu__item'>about</button>
-      <button className='c-desktop-menu__item'>experience</button>
-      <button className='c-desktop-menu__item'>blog</button>
-      <button className='c-desktop-menu__item'>hire me</button>
+      <button className='c-desktop-menu__item' onClick={() => scrollDown(homeSection)}>home</button>
+      <button className='c-desktop-menu__item' onClick={() => scrollDown(aboutSection)}>about</button>
+      <button className='c-desktop-menu__item' onClick={() => scrollDown(experienceSection)}>experience</button>
+      <button className='c-desktop-menu__item' onClick={() => scrollDown(contactSection)}>hire me</button>
+      {/* <button className='c-desktop-menu__item'>blog</button> */}
     </div>
   );
 
   const mobileMenu = (
     <div className='c-mobile-menu'>
-      <button>Home</button>
-      <button>about</button>
-      <button>experience</button>
-      <button>blog</button>
-      <button>hire me</button>
+      <div onClick={handleBurgerClick}>
+        <MenuIcon />
+      </div>
+      {isMenuMobileActive && <div className='c-mobile-menu__modal'>
+        <CloseIcon className='c-mobile-menu__close' onClick={handleCloseMenu} />
+        <button className='c-mobile-menu__item' onClick={() => handleMenuOptionClick(homeSection)}>Home</button>
+        <button className='c-mobile-menu__item' onClick={() => handleMenuOptionClick(aboutSection)}>about</button>
+        <button className='c-mobile-menu__item' onClick={() => handleMenuOptionClick(experienceSection)}>experience</button>
+        <button className='c-mobile-menu__item' onClick={() => handleMenuOptionClick(contactSection)}>hire me</button>
+        {/* <button className='c-mobile-menu__item'>blog</button> */}
+      </div>}
     </div>
   );
 
