9f4b9c5bcc2824aa9301b522204759fcae2f6e19
Marco Villanueva <marco.villanueva@globant.com>
2022-07-28 09:38:02 -0500

    feature( Get-in-touch ): Add Validations to contact form

--- src/components/GetInTouch/GetInTouch.js
+++ src/components/GetInTouch/GetInTouch.js
@@ -10,13 +10,22 @@ export const GetInTouch = forwardRef(({}, ref) => {
     reply_to: '',
   });
 
+  const [wasMessageSent, setWasMessageSent] = useState(false);
+  const [isSubmitEnabled, setIsSubmitEnabled] = useState(true);
+  const [finalMessage, setFinalMessage] = useState('')
+
   const onSubmit = (e) => {
+    setIsSubmitEnabled(false);
     e.preventDefault();
     emailjs.send()
       .then((response) => {
+        setWasMessageSent(true);
+        setFinalMessage('Message was successfully sent!');
         console.log('SUCCESS!', response.status, response.text);
       })
       .catch((err) => {
+        setIsSubmitEnabled(true);
+        setFinalMessage('Message could not be sent, try again later');
         console.log('FAILED...', err);
       });
   };
@@ -28,41 +37,42 @@ export const GetInTouch = forwardRef(({}, ref) => {
   return (
     <div className='c-get-in-touch' ref={ref}>
       <div className="c-get-in-touch__title">Get in touch</div>
-
-      <form className="c-get-in-touch__form">
-        <input
-          type='text'
-          name='from_name'
-          placeholder='Your name'
-          value={toSend.from_name}
-          onChange={handleChange}
-        />
-
-        <input
-          type='text'
-          name='reply_to'
-          placeholder='Your email'
-          value={toSend.reply_to}
-          onChange={handleChange}
-        />
-
-        <textarea
-          name="message"
-          placeholder='Your message'
-          value={toSend.message}
-          onChange={handleChange}
-          rows="6" cols="50"
-        />
-        <div className='c-get-in-touch__button-container'>
-          <Button
-            description="Send my message"
-            theme="primary"
-            action="contact"
-            onClick={onSubmit}
+      {wasMessageSent ? <div className='c-get-in-touch__message-sent'>{finalMessage}</div> :
+        <form className="c-get-in-touch__form">
+          <input
+            type='text'
+            name='from_name'
+            placeholder='Your name'
+            value={toSend.from_name}
+            onChange={handleChange}
           />
-        </div>
 
-      </form>
+          <input
+            type='text'
+            name='reply_to'
+            placeholder='Your email'
+            value={toSend.reply_to}
+            onChange={handleChange}
+          />
+
+          <textarea
+            name="message"
+            placeholder='Your message'
+            value={toSend.message}
+            onChange={handleChange}
+            rows="6" cols="50"
+          />
+          <div className='c-get-in-touch__button-container'>
+            <Button
+              description="Send my message"
+              theme="primary"
+              action="contact"
+              onClick={onSubmit}
+              disabled={!isSubmitEnabled}
+            />
+          </div>
+
+        </form>}
     </div>
   );
 });
